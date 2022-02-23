d7d6991b23558b678478f07fafe05d0b9cb97fec
ngochung04 <dangngochung0401@gmail.com>
2022-02-23 16:34:56 +0700

    xóa file thừa

--- containers/Detail/PageBody.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { Box } from "@chakra-ui/react";
-import TabOne from "./Tabs/TabOne";
-import Footer from "../../components/Footer";
-
-const PageBody: FC = () => {
-  return <></>;
-};
-export default PageBody;
